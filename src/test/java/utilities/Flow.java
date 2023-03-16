6778fdd07e4c977e166cf9dac1f7384e094a6c21
codewizer1 <anazarmamatova@gmail.com>
2023-03-15 20:04:37 -0500

    Initial commit

---
+++ src/test/java/utilities/Flow.java
@@ -0,0 +1,35 @@
+package utilities;
+
+import org.openqa.selenium.interactions.Actions;
+
+public class Flow {
+
+    public static void wait(int millisecond){
+        try {
+            Thread.sleep(millisecond);
+        }catch (InterruptedException e){
+            System.out.println("Interruption happened");
+        }
+    }
+
+    public static void scrollDown (int deltaY){
+        Actions actions = new Actions(Driver.getDriver());
+        actions.scrollByAmount(0,deltaY).perform();
+    }
+
+    public static void scrollUp (int deltaY){
+        Actions actions = new Actions(Driver.getDriver());
+        actions.scrollByAmount(0,deltaY).perform();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
